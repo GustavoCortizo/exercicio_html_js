dcc4ba0a5c50755320cb589086b095e502fd1c7a
Turok3299 <gcortizo99@gmail.com>
2024-03-26 19:32:19 -0300

    finish

--- main.js
+++ main.js
@@ -2,35 +2,31 @@ const form = document.getElementById('form-calcular');
 const numeroX = document.getElementById('numero-x');
 const numeroY = document.getElementById('numero-y');
 
-const formEValido = false;
-
-function calcular(numero) {
-    numero = numeroX.value > numeroY.value;
-    return numero;
+function calcular() {
+    const valorX = parseFloat(numeroX.value);
+    const valorY = parseFloat(numeroY.value);
+    return valorX > valorY;
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-const mensagemSucesso = `O valor: <b>${numeroX.value}</b> é maior que o valor:<b>${numeroY.value}</b>`;
-const mensagemError = `O valor: <b>${numeroX.value}</b> é menor que o valor:<b>${numeroY.value}</b>`;
+    const containerMensagemSucesso = document.querySelector('.sucess-message');
+    containerMensagemSucesso.innerHTML = '';
+    containerMensagemSucesso.style.display = 'none';
+    
+    const containerMensagemError = document.querySelector('.error-message');
+    containerMensagemError.innerHTML = '';
+    containerMensagemError.style.display = 'none';
 
+    const mensagemSucesso = `O valor: <b>${numeroX.value}</b> é maior que o valor:<b>${numeroY.value}</b>`;
+    const mensagemError = `O valor: <b>${numeroX.value}</b> é menor que o valor:<b>${numeroY.value}</b>`;
 
-    if (formEValido) {
-        const containerMensagemSucesso = document.querySelector('.sucess-message');
+    if (calcular()) {
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
-    }
-    else {
-        const containerMensagemError = document.querySelector('.error-message');
+    } else {
         containerMensagemError.innerHTML = mensagemError;
         containerMensagemError.style.display = 'block';
     }
-
-    }
-)
-
-
-
-//containerMensagemError.style.display = 'none';
-//containerMensagemSucesso.style.display = 'none';
+});
